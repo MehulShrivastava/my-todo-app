7f98a7c051e8e20d023632c202754b45c9d881d4
MehulShrivastava <mehulshrivastava72@gmail.com>
2023-07-31 17:13:28 +0530

    initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_todo():
+    with open('todo.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+        return todos_local
+
+def write_todo(todos):
+    with open('todo.txt', 'w') as file:
+        file.writelines(todos)
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todo())
